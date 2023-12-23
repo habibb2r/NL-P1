7b760618e1253a5ac26bb92b0a15e3958dd77fee
useless444 <habibhk127@gmail.com>
2023-12-24 03:14:58 +0600

    Utilits test from laptop

--- src/app/middleware/notFound.ts
+++ src/app/middleware/notFound.ts
@@ -2,7 +2,6 @@
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
 
-
 // eslint-disable-next-line no-unused-vars
 const notFound = (req: Request, res: Response, next: NextFunction) => {
     return res.status(httpStatus.NOT_FOUND).json({
--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -1,4 +1,16 @@
 import { TAcademicSemester } from "../academicSemester/academinSemester.interface";
+import { UserModel } from "./user.model";
+
+const findLastStudent = async()=>{
+      const lastStudent = await UserModel.findOne({
+            role: 'student'
+      },{
+            id: 1,
+            _id: 0
+      }).lean();
+
+      return lastStudent?.id? lastStudent.id : undefined
+}
 
 export const generateStudentId = (payload: TAcademicSemester)=>{
       const currentId = (0).toString();
